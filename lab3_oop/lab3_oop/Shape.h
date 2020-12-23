da5d6050d31278c0603193f3c51069eb9669e8da
ostapshutenko <ostapshutenko@icloud.com>
2020-12-23 13:30:03 +0300

    Add files via upload

---
+++ lab3_oop/lab3_oop/Shape.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Point.h"
+
+class Shape
+{
+protected:
+	Point* _point_arr;
+	char _Id_type;
+public:
+
+		
+
+	virtual ~Shape() {}
+
+	
+	virtual std::string info() = 0;
+	//virtual void info() = 0;
+
+	virtual double getArea()
+	{
+		return 0;
+	}
+
+	
+	virtual void Rotate(double rotation_angle) {};
+
+	
+	virtual void Move(double move_x, double move_y) {};
+
+	
+	static Shape* createShape(char c, Point* pt);
+};
+
